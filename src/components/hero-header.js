e62ca132a4cf602d796788ecb4a6c7b0786d7dc7
James <jameskolean@gmail.com>
2020-04-15 10:23:41 -0400

    checkpoint

--- src/components/hero-header.js
+++ src/components/hero-header.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
-export default () => (
+//import styled from 'styled-components'
+
+// import BackgroundImage from 'gatsby-background-image'
+
+const HeroHeader = () => (
   <StaticQuery
     query={graphql`
       query HeadingQuery {
@@ -14,13 +18,28 @@ export default () => (
         }
       }
     `}
-    render={data => (
+    render={(data) => (
+      // <BackgroundImage
+      //   Tag='section'
+      //   className={className}
+      //   fluid={imageData}
+      //   backgroundColor={`#040e18`}
+      // >
       <div className='hero-header'>
         <div className='headline'>{data.site.siteMetadata.home.title}</div>
         <div className='primary-content'>
           <p>{data.site.siteMetadata.home.description}</p>
         </div>
       </div>
+      // </BackgroundImage>
     )}
   />
 )
+// const StyledHeroHeader = styled(HeroHeader)`
+//   width: 100%;
+//   background-position: bottom center;
+//   background-repeat: repeat-y;
+//   background-size: cover;
+// `
+// export default StyledHeroHeader
+export default HeroHeader
