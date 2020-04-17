b17bedad68558ecbb235c4b67d671f17b04abf79
James <jameskolean@gmail.com>
2020-04-17 18:04:11 -0400

    Google analytics GDRP solution

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,12 +4,23 @@ import { Cookies } from 'react-cookie-consent'
 import ReactGA from 'react-ga'
 
 let initialized = false
+const consentCookieName = 'CookieConsent'
 export const onRouteUpdate = ({ location }, pluginOptions = {}) => {
-  if (Cookies.get('CookieConsent') === `true` && !initialized) {
+  if (
+    Cookies.get(consentCookieName) &&
+    Cookies.get(consentCookieName) === `false`
+  ) {
+    Cookies.remove(consentCookieName)
+    Cookies.remove('_ga')
+    Cookies.remove('_gid')
+    initialized = false
+    return
+  }
+  if (Cookies.get(consentCookieName) === `true` && !initialized) {
     ReactGA.initialize('UA-163801125-1')
     initialized = true
   }
-  if (Cookies.get('CookieConsent') === `true` && initialized) {
+  if (Cookies.get(consentCookieName) === `true` && initialized) {
     ReactGA.set({ page: location.pathname, anonymizeIp: true })
     ReactGA.pageview(location.pathname)
   }
