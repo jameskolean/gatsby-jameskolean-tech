a9974cc0c37d279f16b92c191c0b42a6eb9a4941
James <jameskolean@gmail.com>
2020-04-17 17:11:05 -0400

    test GDRP

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,14 +6,16 @@ import ReactGA from 'react-ga'
 export const onClientEntry = (_, pluginOptions = {}) => {
   console.log('onClientEntry------------------------------------------')
   console.log('CookieConsent', Cookies.get('CookieConsent'))
-  if (Cookies.get('CookieConsent')) {
-    if (!Cookies.get('_ga')) {
-      ReactGA.initialize('UA-163801125-1')
-      console.log('initialized', Cookies.get())
-    }
+  if (Cookies.get('CookieConsent') === `true` && !ReactGA.ga) {
+    ReactGA.initialize('UA-163801125-1')
+    console.log('just called ReactGA.initialize', ReactGA.ga)
   }
 }
 export const onRouteUpdate = ({ location }, pluginOptions = {}) => {
+  if (Cookies.get('CookieConsent') === `true` && !ReactGA.ga) {
+    ReactGA.initialize('UA-163801125-1')
+    console.log('just called ReactGA.initialize again', ReactGA.ga)
+  }
   if (Cookies.get('CookieConsent') === `true` && ReactGA.ga) {
     console.log('track me', location.pathname)
     ReactGA.set({ page: location.pathname })
