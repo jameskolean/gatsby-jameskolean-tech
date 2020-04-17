eef9427d1bb2537c876ff4115a9d148e39d587db
James <jameskolean@gmail.com>
2020-04-17 15:45:30 -0400

    test GDRP

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,17 +68,17 @@ module.exports = {
         ],
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     // The property ID; the tracking code won't be generated without it
-    //     trackingId: 'UA-163801125-1',
-    //     head: true,
-    //     anonymize: true,
-    //      respectDNT: true,
-    //     cookieDomain: 'jameskolean.tech',
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-163801125-1',
+        head: true,
+        anonymize: true,
+        respectDNT: true,
+        cookieDomain: 'jameskolean.tech',
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
@@ -100,25 +100,6 @@ module.exports = {
       resolve: `gatsby-plugin-offline`,
       options: {
         precachePages: [`/todos/`, `/posts/`, `/posts/*`],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics-gdpr`,
-      options: {
-        // The property ID; the tracking code won't be generated without it.
-        trackingId: 'UA-163801125-1',
-        // Optional parameter (default false) - Enable analytics in development mode.
-        enableDevelopment: false, // default false
-        // Optional parameter (default true) - Some countries (such as Germany) require you to use the _anonymizeIP function for Google Analytics. Otherwise you are not allowed to use it.
-        anonymizeIP: true,
-        // Optional parameter (default false) - Starts google analytics with cookies enabled. In some countries (such as Germany) this is not allowed.
-        autoStartWithCookiesEnabled: false,
-        // Optional parameter - Configuration for react-ga and google analytics
-        reactGaOptions: {
-          gaOptions: {
-            sampleRate: 10,
-          },
-        },
       },
     },
   ],
